51e700c7e6914316f4301c47bdb54a18519454cc
toufiq <you@example.com>
2026-03-10 22:07:00 -0300

    Saad's Flake.nix

---
+++ cfg/devshell/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Flake-based Nix devshell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              wordpress
+            ];
+          };
+        }
+      );
+    };
+}
+  
