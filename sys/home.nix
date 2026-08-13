d3a46dce59fcb5dd663940b28e29b4cf90845dc2
toufiq <you@example.com>
2026-08-13 10:28:23 -0300

    virtualisation in container.nix now

--- sys/home.nix
+++ sys/home.nix
@@ -137,41 +137,7 @@ in
 
   };
 };
-virtualisation = {
-  containers = {
-    enable = true;
-    registries.search = [ "docker.io" ];
-  };
-  docker = {
-    enable = false;
-    rootless = {
-      enable = false;
-      setSocketVariable = true;
-      daemon.settings = {
-        data-root = "~/.local/docker";
-        dns = [
-          "1.1.1.1"
-          "1.0.0.1"
-        ];
-        registry-mirrors = [ "https://mirror.gcr.io" ];
-        pruning = {
-          enabled = true;
-          interval = "48h";
-        };
-      };
-    };
-  };
-  podman = {
-    enable = true;
-    dockerCompat = true;
-    defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
-    autoPrune = {
-      enable = true;
-      dates = "weekly";
-      flags = [ "--all" ];
-    };
-  };
-};
+
 xdg.portal = {
   enable = true;
   extraPortals = with pkgs;
